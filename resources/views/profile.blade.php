c4e654f02fcceacbdb47e52d3038683f8c3a9102
Indah Kusuma Ningrum <indahkusumaningrum345@gmail.com>
2024-09-13 07:25:02 +0700

    perubahan kecil

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman Profi</title>
+    <title>Halaman Profile</title>
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -53,7 +53,7 @@
     </style>
 </head>
 <body>
-    <h1>Biodata</h1>
+    <h1>Myself</h1>
 
     <div class="profile-container">
         <img src="{{ asset('images/foto.png') }}" alt="Profile Picture" class="profile-img">
